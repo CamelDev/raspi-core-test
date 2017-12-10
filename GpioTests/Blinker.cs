74b0008fd6bf2f8a1a5c3fd966f6fc9e1282c38e
Kamil Dabrowski <kamil.dabrowski@gmail.com>
2017-12-10 13:06:37 +0100

    Hello world ready

---
+++ GpioTests/Blinker.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace raspitest.Gpio
+{
+    class Blinker
+    {
+        static void Blink(int pinNumber, int count)
+        {
+            Console.WriteLine("Blinking!");
+        }
+    }
+}
