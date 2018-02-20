5c49b22672d399a5b1fad0bc140e4c4098a9f4df
Kamil Dabrowski <kamil.dabrowski@gmail.com>
2018-02-20 12:21:47 +0100

    Set output mode before write

--- Gpio/RaspiPin.cs
+++ Gpio/RaspiPin.cs
@@ -9,8 +9,12 @@ namespace CamelDev.AutoCtrl.Device.Gpio
 
         public void SetPinValue(BcmIo pin, PinValue value)
         {
-            // TODO: set mode to output if possible!
-            GetGpioPinByBcmNumber(pin).Write(value == PinValue.High ? true : false);
+             var gpioPin = GetGpioPinByBcmNumber(pin);
+             
+             if(gpioPin.PinMode == GpioPinDriveMode.Input)
+                gpioPin.PinMode = GpioPinDriveMode.Output;
+
+             gpioPin.Write(value == PinValue.High ? true : false);
         }
 
         public PinValue GetPinValue(BcmIo pin)
@@ -24,21 +28,21 @@ namespace CamelDev.AutoCtrl.Device.Gpio
 
             switch(pinNumber)
             {
-                //case BcmIo.Pin1: return SCL.0 IN
+                // case BcmIo.Pin1: return SCL.0 IN
                 case BcmIo.Pin2: return Pi.Gpio.Pin08;                
                 // case BcmIo.Pin3: return SDA.1 IN
                 case BcmIo.Pin4: return Pi.Gpio.Pin07;
                 case BcmIo.Pin5: return Pi.Gpio.Pin21;
                 case BcmIo.Pin6: return Pi.Gpio.Pin22;
-                //case BcmIo.Pin7: return CE1 IN
-                //case BcmIo.Pin8: return CE0 OUT
+                // case BcmIo.Pin7: return CE1 IN
+                // case BcmIo.Pin8: return CE0 OUT
                 // case BcmIo.Pin9: return MOS0 IN;
-                //case BcmIo.Pin10: return MOSI OUT;
+                // case BcmIo.Pin10: return MOSI OUT;
                 // case BcmIo.Pin11: return SCLK IN;
                 case BcmIo.Pin12: return Pi.Gpio.Pin26;                    
                 case BcmIo.Pin13: return Pi.Gpio.Pin23;
-                //case BcmIo.Pin14: return TxD ALT0
-                //case BcmIo.Pin15: return RxD ALT0;
+                // case BcmIo.Pin14: return TxD ALT0
+                // case BcmIo.Pin15: return RxD ALT0;
                 case BcmIo.Pin16: return Pi.Gpio.Pin27;
                 case BcmIo.Pin17: return Pi.Gpio.Pin00;
                 case BcmIo.Pin18: return Pi.Gpio.Pin01;
